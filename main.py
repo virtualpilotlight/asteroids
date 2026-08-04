30f53abb51d9c3629de9ebce210d811fd9459e01
virtualpilotlight <omega.x.rae@gmail.com>
2026-08-04 16:19:01 -0700

    clock added

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from logger import log_state
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0.0
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -21,6 +23,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
