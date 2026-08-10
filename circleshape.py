a98508e8f9fc39846c2c005b435b8d37be461309
virtualpilotlight <omega.x.rae@gmail.com>
2026-08-10 13:55:13 -0700

    collides_with working

--- circleshape.py
+++ circleshape.py
@@ -1,6 +1,5 @@
 import pygame
 
-
 # Base class for game objects
 class CircleShape(pygame.sprite.Sprite):
     containers: tuple[pygame.sprite.Group, ...]
@@ -23,3 +22,7 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt: float) -> None:
         # must override
         pass
+
+    def collides_with(self, other: "CircleShape") -> bool:
+        distance = self.position.distance_to(other.position)
+        return distance <= self.radius + other.radius
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from sys import displayhook
 import threading
 import pygame
-import asteroids
+import asteroid
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS, LINE_WIDTH
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
-from asteroids import Asteroid
+from asteroid import Asteroid
 from asteroidfield import AsteroidField
 
 def main():
@@ -36,6 +36,11 @@ def main():
             pass
         screen.fill("black")
         updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player_ch):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
         for thing in drawable:
             thing.draw(screen)
         pygame.display.flip()
