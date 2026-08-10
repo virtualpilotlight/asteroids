e4e700c7bec8fde8e1ce22dc97e54ab3732f2a22
virtualpilotlight <omega.x.rae@gmail.com>
2026-08-09 22:02:38 -0700

    movement working

--- player.py
+++ player.py
@@ -2,7 +2,7 @@ from mimetypes import init
 
 import pygame
 from pygame.transform import rotate
-from constants import PLAYER_RADIUS, LINE_WIDTH, SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_TURN_SPEED, PLAYER_SPEED
 from circleshape import CircleShape
 
 class Player(CircleShape):
@@ -34,3 +34,13 @@ class Player(CircleShape):
             self.rotate(-abs(dt))
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-abs(dt))
+
+    def move(self, dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
