b6cdde084041654c0710e8dd071788980f9752d2
virtualpilotlight <omega.x.rae@gmail.com>
2026-08-11 01:23:45 -0700

    adding shot

--- player.py
+++ player.py
@@ -44,3 +44,8 @@ class Player(CircleShape):
         rotated_vector = unit_vector.rotate(self.rotation)
         rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
         self.position += rotated_with_speed_vector
+
+    def shoot():
+        shot = self.position
+        shot.velosity
+        pygame.Vector2(0, 1)
